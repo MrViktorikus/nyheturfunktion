4ad29b0f90114205e7d8dcd4b2a505ab90c5263e
Johan Kellén <johan.kellen@gmail.com>
2015-03-31 10:25:40 +0200

    login.php klar

--- login.php
+++ login.php
@@ -5,15 +5,12 @@
         <title>login.php</title>
     </head>
     <body>
-        <?php
-        echo "<form method = 'POST'>
-        <p>Användarnamn:</p> <input type = 'text' name = 'anvnam' required>
-        <p>Lösenord:</p><input type = 'password' name = 'losord' required><br>
-        <input type='checkbox' name='checkbox'> Håll mig inloggad<br>
-        <input type = 'submit' value = 'Logga in'>
-        </form>";
-            echo "<form method='POST'> <input type = 'submit' value = 'Glömt Lösenord?' name='glomt'></form>";
-        
-        ?>
+        <form method = 'POST' action="doLogin.php">
+            <p>Användarnamn:</p> <input type = 'text' name = 'anvnam' required>
+            <p>Lösenord:</p><input type = 'password' name = 'losord' required><br>
+            <input type='checkbox' name='checkbox'> Håll mig inloggad<br>
+            <input type = 'submit' value = 'Logga in'>
+        </form>
+        <form method='POST'> <input type = 'submit' value = 'Glömt Lösenord?' name='glomt'></form>
     </body>
 </html>

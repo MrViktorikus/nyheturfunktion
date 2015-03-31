9b33e66e9659be791fb0adbda514a630bc25cae9
Johan Kellén <johan.kellen@gmail.com>
2015-03-31 10:56:40 +0200

    Enkel login fungerar nu. Sparar ej i kaka.

--- doLogIn.php
+++ doLogIn.php
@@ -1,16 +1,13 @@
 <?php
 
-define("DB_SERVER", "localhost");
-define("DB_USER", "root");
-define("DB_PASSWORD", "");
-define("DB_NAME", "berzanapp");
-$dbh = new PDO('mysql:dbname=' . DB_NAME . ';host=' . DB_SERVER . ';charset=utf8', DB_USER, DB_PASSWORD);
-session_start();
+require_once 'db.php';
+require_once 'session.php';
 
 //if (isset($_POST["anvnamn"])) {
     $anvnamn = filter_input(INPUT_POST, 'anvnamn', FILTER_SANITIZE_SPECIAL_CHARS);
     $losord = filter_input(INPUT_POST, 'losord', FILTER_SANITIZE_SPECIAL_CHARS);
     $sql = "SELECT * FROM `users` WHERE anvnamn='$anvnamn' AND losenord='$losord'";
+
 //    echo $sql;
     $stmt = $dbh->prepare($sql);
 //    $stmt->bindParam(":anvnamn", $anvnamn);
@@ -19,11 +16,9 @@ session_start();
     $login = $stmt->fetch();
 //    var_dump($login);
     if (!empty($login)) {
-
-//        $_SESSION["inlog"] = 1;
         $_SESSION["anvnamn"] = $login["anvnamn"];
-//        $_SESSION["id"] = $login["id"];
+
 //        var_dump($_SESSION);
     }
-//}
+
 header('Location: index.php');
--- login.php
+++ login.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <form method = 'POST' action="doLogin.php">
-            <p>Användarnamn:</p> <input type = 'text' name = 'anvnam' required>
+            <p>Användarnamn:</p> <input type = 'text' name = 'anvnamn' required>
             <p>Lösenord:</p><input type = 'password' name = 'losord' required><br>
             <input type='checkbox' name='checkbox'> Håll mig inloggad<br>
             <input type = 'submit' value = 'Logga in'>

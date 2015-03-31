6c6970538516b757b46394e93f058887f7159e9b
Johan Kellén <johan.kellen@gmail.com>
2015-03-31 10:56:09 +0200

    Skapade include-filer db och session
    dbm omdöpt till dbh för svenska och gemensam namngivning. Kan ge
    problem.

--- db.php
+++ db.php
@@ -3,7 +3,9 @@
 define("DB_SERVER", "localhost");
 define("DB_USER", "root");
 define("DB_PASSWORD", "");
-define("DB_NAME", "berzapp");
+define("DB_NAME", "berzanapp");
+
 $dsn = 'mysql:dbname=' . DB_NAME . ';host=' . DB_SERVER . ';charset=utf8';
 $attributes = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
-$dbm = new PDO($dsn, DB_USER, DB_PASSWORD, $attributes);
+$dbh = new PDO($dsn, DB_USER, DB_PASSWORD, $attributes);
+//$dbm = new PDO($dsn, DB_USER, DB_PASSWORD, $attributes);
